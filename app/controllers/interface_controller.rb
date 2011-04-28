71b68d93b97c239fd7b1b0b31e0ac66aa944cdc9
Dane Pieri <dpieri@cmu-435163.wv.cc.cmu.edu>
2011-04-28 00:03:48 -0400

    removed css controller and created a colors helper

--- app/controllers/interface_controller.rb
+++ app/controllers/interface_controller.rb
@@ -35,19 +35,19 @@ class InterfaceController < ApplicationController
   def colors(is_raw_color)
     if is_raw_color
       params[:accent_color] ? @color_1_a = params[:accent_color].split(',') : @color_1_a = @@color_1
-      params[:button_color] ? @color_2_a = params[:button_color].split(',') : @color_2_a = @@color_2
-      params[:background_color] ? @color_3_a = params[:background_color].split(',') : @color_3_a = @@color_3
+      params[:button_color] && params[:button_color_customized] == 'true' ? @color_2_a = params[:button_color].split(',') : @color_2_a = @@color_2
+      params[:background_color] && params[:button_color_customized] == 'true' ? @color_3_a = params[:background_color].split(',') : @color_3_a = @@color_3
     else
-      params[:accent_color] ? @color_1_a = extract_color_from_string(params[:accent_color]) : @color_1_a = @@color_1
-      params[:button_color] ? @color_2_a = extract_color_from_string(params[:button_color]): @color_2_a = @@color_2
-      params[:background_color] ? @color_3_a = extract_color_from_string(params[:background_color]): @color_3_a = @@color_3
+      params[:accent_color] ? @color_1_a = ColorHelper::extract_color_from_string(params[:accent_color]) : @color_1_a = @@color_1
+      params[:button_color] && params[:button_color_customized] == 'true' ? @color_2_a = ColorHelper::extract_color_from_string(params[:button_color]): @color_2_a = @@color_2
+      params[:background_color] && params[:button_color_customized] == 'true' ? @color_3_a = ColorHelper::extract_color_from_string(params[:background_color]): @color_3_a = @@color_3
     end
 
     @color_1 = @color_1_a.join(',')
     @color_2 = @color_2_a.join(',')
     @color_3 = @color_3_a.join(',')
     
-    color = rgb_to_hsv @color_1_a
+    color = ColorHelper::rgb_to_hsv @color_1_a
     
     h1 = (color[0] + 320) % 360  #80
     h2 = (color[0] + 140) % 360 #260
@@ -57,17 +57,25 @@ class InterfaceController < ApplicationController
     # @color_1_a = hsvToRGB(color_1_hsv)
     # @color_1 = @color_1_a.join(',')
     
-    color_2_hsv = [h2, color[1], color[2]]
-    @color_2_a = hsvToRGB(color_2_hsv)
-    @color_2 = @color_2_a.join(',')
+    color_2_hsv = ColorHelper::rgb_to_hsv @color_2.split(',')
+    unless params[:button_color_customized] == 'true'
+      color_2_hsv = [h2, color[1], color[2]]
+      @color_2_a = ColorHelper::hsvToRGB(color_2_hsv)
+      @color_2 = @color_2_a.join(',')
+    end
     
-    color_3_hsv = [h1, color[1], color[2]]
-    @color_3_a =  hsvToRGB(color_3_hsv)
-    @color_3 = @color_3_a.join(',')
+    color_3_hsv = ColorHelper::rgb_to_hsv @color_3.split(',')
+    unless params[:background_color_customized] == 'true'
+      color_3_hsv = [h1, color[1], color[2]]
+      @color_3_a =  ColorHelper::hsvToRGB(color_3_hsv)
+      @color_3 = @color_3_a.join(',')
+    end
     
-    @color_3_dark = hsvToRGB(darken @color_3_a, 0.40)
+    @color_3_dark = ColorHelper::hsvToRGB(darken @color_3_a, 0.40)
     
     @dark_background = color_3_hsv[1] > 0.8
+    @background_color_customized = params[:background_color_customized] == 'true'
+    @button_color_customized = params[:button_color_customized] == 'true'
     
     #
   	# Gradient
@@ -84,8 +92,8 @@ class InterfaceController < ApplicationController
       color_diff = 0
     end
 
-    @dark_color = hsvToRGB(darken color_2_hsv, 2*color_diff).join(',')
-    @light_color = hsvToRGB(darken color_2_hsv, -color_diff).join(',')
+    @dark_color = ColorHelper::hsvToRGB(darken color_2_hsv, 2*color_diff).join(',')
+    @light_color = ColorHelper::hsvToRGB(darken color_2_hsv, -color_diff).join(',')
     
     
     #
@@ -166,86 +174,6 @@ class InterfaceController < ApplicationController
   	# Top Nav
   	#
   	@top_nav = @@top_nav[@site_type.to_sym]
-  end
-  
-  
-  # Converts an RGB color value to HSV. Conversion formula
-  # adapted from http://en.wikipedia.org/wiki/HSV_color_space.
-  # Assumes r, g, and b are contained in the set [0, 255] and
-  # returns h, s, and v in the set [0, 1].
-  # 
-  # @param   Number  r       The red color value
-  # @param   Number  g       The green color value
-  # @param   Number  b       The blue color value
-  # @return  Array           The HSV representation
-  def rgb_to_hsv(color)
-    r = color[0].to_f
-    g = color[1].to_f
-    b = color[2].to_f
-    
-    r = r/255 and g = g/255 and b = b/255
-    
-    max = [r, g, b].max
-    min = [r, g, b].min
-    h, s, v = max, max, max
-
-    d = max - min
-    s = max == 0 ? 0 : d / max
-    
-    if max == min
-      h = 0 # achromatic
-    else
-      case max
-      when r
-        h = (g - b) / d + (g < b ? 6 : 0)
-      when g
-        h = (b - r) / d + 2
-      when b
-        h = (r - g) / d + 4
-      end
-      
-      h = h*0.6
-    end
-    
-    [h*100, s, v]
-  end
-  
-  def hsvToRGB(hsv)
-		h, s, v = hsv
-		hi = (h/60.0).floor % 6
-		f = h/60.0 - (h/60.0).floor
-		p = (v * (1 - s))
-		q = (v * (1 - f * s))
-		t = (v * (1 - (1 - f) * s) )
-		
-    v = (v*255).to_i
-    t = (t*255).to_i
-    p = (p*255).to_i
-    q = (q*255).to_i
-		@rgb = []
-		case hi
-			when 0
-        @rgb = [v, t, p]
-			when 1
-				@rgb = [q, v, p]
-			when 2
-				@rgb = [p, v, t]
-			when 3
-				@rgb = [p, q, v]
-			when 4
-				@rgb = [t, p, v]
-			when 5
-        @rgb = [v, p, t]
-  	end
-  	return @rgb
-	end
-  
-  def extract_color_from_string(input)
-    input = input.sub('rgb(', '').sub(')', '').split(',')
-    input.each_with_index do |c,i|
-      input[i] = c.to_i
-    end
-    input
   end
 
 end
---
+++ app/helpers/color_helper.rb
@@ -0,0 +1,82 @@
+module ColorHelper
+  
+  # Converts an RGB color value to HSV. Conversion formula
+  # adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+  # Assumes r, g, and b are contained in the set [0, 255] and
+  # returns h, s, and v in the set [0, 1].
+  # 
+  # @param   Number  r       The red color value
+  # @param   Number  g       The green color value
+  # @param   Number  b       The blue color value
+  # @return  Array           The HSV representation
+  def self.rgb_to_hsv(color)
+    r = color[0].to_f
+    g = color[1].to_f
+    b = color[2].to_f
+    
+    r = r/255 and g = g/255 and b = b/255
+    
+    max = [r, g, b].max
+    min = [r, g, b].min
+    h, s, v = max, max, max
+
+    d = max - min
+    s = max == 0 ? 0 : d / max
+    
+    if max == min
+      h = 0 # achromatic
+    else
+      case max
+      when r
+        h = (g - b) / d + (g < b ? 6 : 0)
+      when g
+        h = (b - r) / d + 2
+      when b
+        h = (r - g) / d + 4
+      end
+      
+      h = h*0.6
+    end
+    
+    [h*100, s, v]
+  end
+  
+  def self.hsvToRGB(hsv)
+		h, s, v = hsv
+		hi = (h/60.0).floor % 6
+		f = h/60.0 - (h/60.0).floor
+		p = (v * (1 - s))
+		q = (v * (1 - f * s))
+		t = (v * (1 - (1 - f) * s) )
+		
+    v = (v*255).to_i
+    t = (t*255).to_i
+    p = (p*255).to_i
+    q = (q*255).to_i
+		@rgb = []
+		case hi
+			when 0
+        @rgb = [v, t, p]
+			when 1
+				@rgb = [q, v, p]
+			when 2
+				@rgb = [p, v, t]
+			when 3
+				@rgb = [p, q, v]
+			when 4
+				@rgb = [t, p, v]
+			when 5
+        @rgb = [v, p, t]
+  	end
+  	return @rgb
+	end
+  
+  def self.extract_color_from_string(input)
+    input = input.sub('rgb(', '').sub(')', '').split(',')
+    input.each_with_index do |c,i|
+      input[i] = c.to_i
+    end
+    input
+  end
+  
+end
